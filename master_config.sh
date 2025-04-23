14f12ed0f2641863e6ffd0bab08a3e3943e0b89a
Kyrozn <95051954+Kyrozn@users.noreply.github.com>
2025-04-23 23:12:34 +0200

    init shell script for config

---
+++ master_config.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Mettre à jour les paquets
+echo "Mise à jour des paquets..."
+sudo apt update && sudo apt upgrade -y
+
+# Obtenir l'adresse IP de la machine
+echo "Récupération de l'adresse IP de la machine..."
+ip=$(hostname -I | awk '{print $1}')
+echo "Adresse IP de la machine veuillez la noter quelque part: $ip"
+
+# Installer Docker
+echo "Installation de Docker..."
+sudo apt install -y docker.io
+
+# Activer Docker au démarrage
+echo "Activation de Docker au démarrage..."
+sudo systemctl enable docker
+sudo systemctl start docker
+
+# Ajouter l'utilisateur au groupe sudo et docker
+echo "Ajout de l'utilisateur au groupe sudo et docker..."
+sudo usermod -aG sudo $USER
+sudo usermod -aG docker $USER
+newgrp docker
+
+echo "Initialisation du manager swarm"
+docker swarm init --advertise-addr $ip
+echo "Token pour les workers veillez a le noter:"
+docker swarm join-token worker -q
+
+echo "Installation du volume partagé"
+sudo apt install nfs-kernel-server -y
+sudo mkdir -p /mnt/mc-data
+sudo chown nobody:nogroup /mnt/mc-data
+
+echo "/mnt/mc-data *(rw,sync,no_subtree_check)" | sudo tee -a /etc/exports
+sudo exportfs -a
+sudo systemctl restart nfs-kernel-server
+
+echo "Script terminé. Veuillez redémarrer votre session pour appliquer toutes les modifications."
